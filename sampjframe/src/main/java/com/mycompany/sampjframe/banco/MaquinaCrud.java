f8ac734255af3e6b34476778a1ac94682020cc0c
DanyloSPTech <danylo.gomes@sptech.school>
2022-10-14 01:04:49 -0300

    2.3.2

--- sampjframe/src/main/java/com/mycompany/sampjframe/banco/MaquinaCrud.java
+++ sampjframe/src/main/java/com/mycompany/sampjframe/banco/MaquinaCrud.java
@@ -4,8 +4,10 @@
  */
 package com.mycompany.sampjframe.banco;
 
+import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 /**
  *
@@ -14,10 +16,11 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class MaquinaCrud {
     
     private JdbcTemplate jbdcTemplate;
+    Maquina maquina = new Maquina();
 //    private Maquina maquina;
 
     public void inserir(){
-        Maquina maquina = new Maquina();
+        
         maquina.gerarSerial();
         maquina.setNome("Terminal 2");
         maquina.setFkEmpresa(1);
@@ -26,6 +29,22 @@ public class MaquinaCrud {
         JdbcTemplate cursor = conexao.getConnection();
         
         cursor.update("INSERT INTO Maquina (serialMaquina, nome, fkEmpresa) VALUES (?, ?, ?);", maquina.getSerialMaquina(), maquina.getNome(), maquina.getFkEmpresa());
+    }
+    
+    public List<Maquina> selectTodasMaquinas(){
+        Conexao conexao = new Conexao();
+        JdbcTemplate cursor = conexao.getConnection();
+        List <Maquina> listaMaquinas;
+        listaMaquinas = cursor.query("SELECT * FROM Maquina", new BeanPropertyRowMapper(Maquina.class));
+        return listaMaquinas;
+    }
+    
+    public List<Maquina> selectMaquinaSerial(String serialMaquina){
+        Conexao conexao = new Conexao();
+        JdbcTemplate cursor = conexao.getConnection();
+        List <Maquina> listaMaquinas;
+        listaMaquinas = cursor.query(String.format("SELECT * FROM Maquina WHERE serialMaquina LIKE '%s'", serialMaquina), new BeanPropertyRowMapper(Maquina.class));
+        return listaMaquinas;
     }
     
 }
--- sampjframe/src/main/java/com/mycompany/sampjframe/banco/UsuarioEmpresaCrud.java
+++ sampjframe/src/main/java/com/mycompany/sampjframe/banco/UsuarioEmpresaCrud.java
@@ -1,6 +1,7 @@
 
 package com.mycompany.sampjframe.banco;
 import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
     
     
@@ -19,6 +20,6 @@ public class UsuarioEmpresaCrud {
         Conexao conexao = new Conexao();
         JdbcTemplate cursor = conexao.getConnection();
         
-      List <UsuarioEmpresaCrud> checagem = cursor.query("SELECT * FROM UsuarioEmpresa WHERE nomeUsuario = ?",usuarioemp.getNomeUsuario());
+      List <UsuarioEmpresaCrud> checagem = cursor.query(String.format("SELECT * FROM UsuarioEmpresa WHERE nomeUsuario LIKE '%s' AND senha LIKE '%s'", usuarioemp.getNomeUsuario(), usuarioemp.getSenha()), new BeanPropertyRowMapper(UsuarioEmpresa.class));
     }
 }
